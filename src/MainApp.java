b394ebd85769e9ecf7dc6bdfde2b9b4235e3a666
Kristen <kcarethers@gmail.com>
2016-11-21 16:47:38 -0500

    Changing constructor in Product class

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,24 +1,29 @@
 import java.util.ArrayList;
 
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+// Create arraylist with name,price, tax
 		ArrayList<Product> products = new ArrayList <Product>();
-		products.add(new Product("Book", 12.49, 0));
-		products.add(new Product("Music CD", 14.49, .10));
-		products.add(new Product("Chocolate Bar",.85, 0));
-		products.add(new Product("Imported Box of Chocolates1", 10.00, .05));
-		products.add(new Product("Imported Bottle of Perfume1", 47.50, .15));
-		products.add(new Product("Imported Bottle of Perfume2", 27.99, .15));
-		products.add(new Product("Bottle of Perfume", 18.99,.10 ));
-		products.add(new Product("Packet of Headache Pills", 9.75, 0 ));
-		products.add(new Product("Box of Imported Chocolates2", 11.25, .05 ));
+		products.add(new Product("Book", 12.49, "none"));
+		products.add(new Product("Music CD", 14.49, "goods"));
+		products.add(new Product("Chocolate Bar",.85, "none"));
+		products.add(new Product("Imported Box of Chocolates1", 10.00, "import"));
+		products.add(new Product("Imported Bottle of Perfume1", 47.50, "other"));
+		products.add(new Product("Imported Bottle of Perfume2", 27.99, "other"));
+		products.add(new Product("Bottle of Perfume", 18.99,"goods" ));
+		products.add(new Product("Packet of Headache Pills", 9.75, "none" ));
+		products.add(new Product("Box of Imported Chocolates2", 11.25, "import" ));
 		
 		ArrayList<Product> shoppingCart = new ArrayList <>();
 		
-		System.out.println(products.get(0));
+		//System.out.println(products.get(0));
+		
+		if(product.get(i).getCategoryTax.contentEquals ("none"){
+			double tax = getItemPrice * 
+		}
 	}
 	
 //1. ArrayList - product name and price 
