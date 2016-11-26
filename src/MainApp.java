80bd6a2a255b71bc911d66a95b8d2de2703131be
Kristen <kcarethers@gmail.com>
2016-11-26 12:25:04 -0500

    Got rid of some test code

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,114 +1,124 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
-
 public class MainApp {
 
 	public static void main(String[] args) {
-		
-		
-// Create arraylist with name,price, tax
-		ArrayList<Product> products = new ArrayList <Product>();
+
+		// Create arraylist with name,price, tax
+		ArrayList<Product> products = new ArrayList<Product>();
 		products.add(new Product(true, "Book", 12.49, false));
 		products.add(new Product(false, "Music CD", 14.99, false));
-		products.add(new Product(true, "Chocolate Bar",.85, false));
-		products.add(new Product(true, "Imported Box of Chocolates1", 10.00,true));
-		products.add(new Product(false, "Imported Bottle of Perfume1", 47.50, true));
-		products.add(new Product(false, "Imported Bottle of Perfume2", 27.99, true));
-		products.add(new Product(false, "Bottle of Perfume", 18.99, false ));
-		products.add(new Product(true, "Packet of Headache Pills", 9.75, false ));
-		products.add(new Product(true, "Box of Imported Chocolates2", 11.25, true ));
-		
-		//created shopping cart for Input 1 
-		ArrayList<Product> shoppingCart1 = new ArrayList <Product>();
-		//added products to shopping cart for input 1
+		products.add(new Product(true, "Chocolate Bar", .85, false));
+		products.add(new Product(true, "Imported Box of Chocolates1", 10.00,
+				true));
+		products.add(new Product(false, "Imported Bottle of Perfume1", 47.50,
+				true));
+		products.add(new Product(false, "Imported Bottle of Perfume2", 27.99,
+				true));
+		products.add(new Product(false, "Bottle of Perfume", 18.99, false));
+		products.add(new Product(true, "Packet of Headache Pills", 9.75, false));
+		products.add(new Product(true, "Box of Imported Chocolates2", 11.25,
+				true));
+
+		// created shopping cart for Input 1
+		ArrayList<Product> shoppingCart1 = new ArrayList<Product>();
+		// added products to shopping cart for input 1
 		shoppingCart1.add(products.get(0));
 		shoppingCart1.add(products.get(1));
 		shoppingCart1.add(products.get(2));
-		
-		//each product calculate sales tax
+
+		// each product calculate sales tax
 		BigDecimal taxTotal = BigDecimal.valueOf(0);
-		for (int i =0; i<shoppingCart1.size(); i++){
-			//sum all taxes for each product
-			//taxTotal = taxTotal.add(p.getTax());
+		for (int i = 0; i < shoppingCart1.size(); i++) {
+			// sum all taxes for each product
+			// taxTotal = taxTotal.add(p.getTax());
 			taxTotal = taxTotal.add(products.get(i).getTax());
-			//TEST  System.out.println(products.get(i).getItemName());	
-			
+			// TEST System.out.println(products.get(i).getItemName());
+
 		}
 		BigDecimal sum = new BigDecimal(0);
 
-		for (int i =0; i<shoppingCart1.size(); i++){
+		for (int i = 0; i < shoppingCart1.size(); i++) {
 
-			//Getting tax for every item. 
-		System.out.println("1 " + shoppingCart1.get(i).getItemName()+ ": " + shoppingCart1.get(i).getItemPrice().add(shoppingCart1.get(i).getTax()));
-		//going through loop and adding all items together, BigDecimal 
-		sum = sum.add(shoppingCart1.get(i).getItemPrice().add(shoppingCart1.get(i).getTax()));
-	
+			// Getting tax for every item.
+			System.out.println("1 "
+					+ shoppingCart1.get(i).getItemName()
+					+ ": "
+					+ shoppingCart1.get(i).getItemPrice()
+							.add(shoppingCart1.get(i).getTax()));
+			// going through loop and adding all items together, BigDecimal
+			sum = sum.add(shoppingCart1.get(i).getItemPrice()
+					.add(shoppingCart1.get(i).getTax()));
+
 		}
 		System.out.println("Sales Taxes: " + taxTotal);
 		System.out.println("Grand Total: " + sum);
 
-	
-		
-		      //created shopping cart for Input 2 
-				ArrayList<Product> shoppingCart2 = new ArrayList <Product>();
-				//added products to shopping cart for Input 2
-				shoppingCart2.add(products.get(3));
-				shoppingCart2.add(products.get(4));
-				
-				
-				//each product calculate sales tax
-				BigDecimal taxTotal1 = BigDecimal.valueOf(0);
-				for (int x =0; x<shoppingCart2.size(); x++){
-					//sum all taxes for each product
-					//taxTotal = taxTotal.add(p.getTax());
-					taxTotal1 = taxTotal1.add(shoppingCart2.get(x).getTax());
-				//TEST  System.out.println(products.get(i).getItemName());
-					
-				}
-				BigDecimal sum1 = new BigDecimal(0);
+		// created shopping cart for Input 2
+		ArrayList<Product> shoppingCart2 = new ArrayList<Product>();
+		// added products to shopping cart for Input 2
+		shoppingCart2.add(products.get(3));
+		shoppingCart2.add(products.get(4));
 
-				for (int x =0; x<shoppingCart2.size(); x++){
+		// each product calculate sales tax
+		BigDecimal taxTotal1 = BigDecimal.valueOf(0);
+		for (int x = 0; x < shoppingCart2.size(); x++) {
+			// sum all taxes for each product
+			// taxTotal = taxTotal.add(p.getTax());
+			taxTotal1 = taxTotal1.add(shoppingCart2.get(x).getTax());
+			// TEST System.out.println(products.get(i).getItemName());
 
-					//Getting tax for every item. 
-				System.out.println("1 " + shoppingCart2.get(x).getItemName()+ ": " + shoppingCart2.get(x).getItemPrice().add(shoppingCart2.get(x).getTax()));
-				//going through loop and adding all items together, BigDecimal 
-				sum1 = sum1.add(shoppingCart2.get(x).getItemPrice().add(shoppingCart2.get(x).getTax()));
-				}
-				System.out.println("Sales Taxes: " + taxTotal1);
-				System.out.println("Grand Total: " + sum1 );
-				
-				
-				//created shopping cart for Input 3 
-				ArrayList<Product> shoppingCart3 = new ArrayList <Product>();
-				//added products to shopping cart for Input 3
-				shoppingCart3.add(products.get(5));
-				shoppingCart3.add(products.get(6));
-				shoppingCart3.add(products.get(7));
-				shoppingCart3.add(products.get(8));
-				
-				//each product calculate sales tax
-				BigDecimal taxTotal2 = BigDecimal.valueOf(0);
-				for (int i =0; i<shoppingCart3.size(); i++){
-					//sum all taxes for each product
-					//taxTotal = taxTotal.add(p.getTax());
-					taxTotal2 = taxTotal2.add(shoppingCart3.get(i).getTax());
-					//TEST System.out.println(products.get(i).getItemName());	
-				}
-				BigDecimal sum2 = new BigDecimal(0);
+		}
+		BigDecimal sum1 = new BigDecimal(0);
 
-				for (int i =0; i<shoppingCart3.size(); i++){
+		for (int x = 0; x < shoppingCart2.size(); x++) {
 
-					//Getting tax for every item. 
-				System.out.println("1 " + shoppingCart3.get(i).getItemName()+ ": " + shoppingCart3.get(i).getItemPrice().add(shoppingCart3.get(i).getTax()));
-				//going through loop and adding all items together, BigDecimal 
-				sum2 = sum2.add(shoppingCart3.get(i).getItemPrice().add(shoppingCart3.get(i).getTax()));
-				}
-				System.out.println("Sales Taxes: " + taxTotal2);
-				System.out.println("Grand Total: " + sum2 );
+			// Getting tax for every item.
+			System.out.println("1 "
+					+ shoppingCart2.get(x).getItemName()
+					+ ": "
+					+ shoppingCart2.get(x).getItemPrice()
+							.add(shoppingCart2.get(x).getTax()));
+			// going through loop and adding all items together, BigDecimal
+			sum1 = sum1.add(shoppingCart2.get(x).getItemPrice()
+					.add(shoppingCart2.get(x).getTax()));
+		}
+		System.out.println("Sales Taxes: " + taxTotal1);
+		System.out.println("Grand Total: " + sum1);
 
-				
+		// created shopping cart for Input 3
+		ArrayList<Product> shoppingCart3 = new ArrayList<Product>();
+		// added products to shopping cart for Input 3
+		shoppingCart3.add(products.get(5));
+		shoppingCart3.add(products.get(6));
+		shoppingCart3.add(products.get(7));
+		shoppingCart3.add(products.get(8));
 
-	
-}
+		// each product calculate sales tax
+		BigDecimal taxTotal2 = BigDecimal.valueOf(0);
+		for (int i = 0; i < shoppingCart3.size(); i++) {
+			// sum all taxes for each product
+			// taxTotal = taxTotal.add(p.getTax());
+			taxTotal2 = taxTotal2.add(shoppingCart3.get(i).getTax());
+			// TEST System.out.println(products.get(i).getItemName());
+		}
+		BigDecimal sum2 = new BigDecimal(0);
+
+		for (int i = 0; i < shoppingCart3.size(); i++) {
+
+			// Getting tax for every item.
+			System.out.println("1 "
+					+ shoppingCart3.get(i).getItemName()
+					+ ": "
+					+ shoppingCart3.get(i).getItemPrice()
+							.add(shoppingCart3.get(i).getTax()));
+			// going through loop and adding all items together, BigDecimal
+			sum2 = sum2.add(shoppingCart3.get(i).getItemPrice()
+					.add(shoppingCart3.get(i).getTax()));
+		}
+		System.out.println("Sales Taxes: " + taxTotal2);
+		System.out.println("Grand Total: " + sum2);
+
+	}
 }
--- src/Product.java
+++ src/Product.java
@@ -1,116 +1,73 @@
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 
- 
 public class Product {
 	// the instance variables
-	    private boolean imprt;
-		private String itemName;
-		private BigDecimal itemPrice;
-		private boolean exempt;
-		private BigDecimal taxRate;
+	private boolean imprt;
+	private String itemName;
+	private BigDecimal itemPrice;
+	private boolean exempt;
+	private BigDecimal taxRate;
 
+	public Product(boolean isExempt, String name, double price, boolean isImport) {
 		
-		public Product(boolean isExempt, String name, double price, boolean isImport) {
-			//in order for me to round with BigDecimal it has to take in a MathContext number in order to have necessary precision to round
-			
-			itemName = name;
-			//itemPrice = new DecimalFormat("#0.##").format(bd);
-			itemPrice = new BigDecimal(price).setScale(2,BigDecimal.ROUND_HALF_DOWN);
-			imprt = isImport;
-			exempt = isExempt;
-			
-			//TEST: System.out.println(itemPrice);
+		itemName = name;
+		// rounding number down and setting to two decimal points
+		itemPrice = new BigDecimal(price).setScale(2,
+				BigDecimal.ROUND_HALF_DOWN);
+		imprt = isImport;
+		exempt = isExempt;
+
+		// TEST: System.out.println(itemPrice);
+	}
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public BigDecimal getItemPrice() {
+		return itemPrice;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Product [imprt=" + imprt + ", itemName=" + itemName
+				+ ", itemPrice=" + itemPrice + ", exempt=" + exempt
+				+ ", taxRate=" + taxRate + "]";
+	}
+
+	// applying tax to each item
+	public BigDecimal getTax() {
+		// holding total sales tax for this product
+		taxRate = new BigDecimal(0);
+		BigDecimal taxItemPrice = new BigDecimal(0);
+
+		if (exempt == false) {
+			// add operator only works with primitives, so used BigDecimal add
+			// method to add 10% tax
+			taxItemPrice = itemPrice.multiply(BigDecimal.valueOf(.10));
+			// taxRate.add(taxRate = new BigDecimal(0););
+		}// close if/else
+
+		if (imprt == true) {
+			// taxItemPrice = itemPrice.multiply(BigDecimal.valueOf(.05));
+			taxItemPrice = taxItemPrice.add(itemPrice.multiply(BigDecimal
+					.valueOf(.05)));
+			// taxRate.add(BigDecimal.valueOf(.05));d
 		}
-		
+		// multiply price times tax to get sales tax for each item
 
-		public boolean isImport() {
-			return imprt;
-		}
+		// System.out.println(taxItemPrice);
+		// convert BigDecimal to double
 
+		taxItemPrice = new BigDecimal(
+				Math.ceil(taxItemPrice.doubleValue() * 20) / 20);
+		taxItemPrice = taxItemPrice.setScale(2, BigDecimal.ROUND_HALF_UP);
 
-		public void setImprt(boolean imprt) {
-			this.imprt = imprt;
-		}
+		return taxItemPrice;
 
+	}
 
-		public String getItemName() {
-			return itemName;
-		}
-
-
-		public void setItemName(String itemName) {
-			this.itemName = itemName;
-		}
-
-
-		public BigDecimal getItemPrice() {
-			return itemPrice;
-		}
-
-
-		public void setItemPrice(BigDecimal itemPrice) {
-			this.itemPrice = itemPrice;
-		}
-
-
-		public boolean isExempt() {
-			return exempt;
-		}
-
-
-		public void setExempt(boolean exempt) {
-			this.exempt = exempt;
-		}
-
-
-		public BigDecimal getTaxRate() {
-			return taxRate;
-		}
-
-
-		public void setTaxRate(BigDecimal taxRate) {
-			this.taxRate = taxRate;
-		}
-
-
-		@Override
-		public String toString() {
-			return "Product [imprt=" + imprt + ", itemName=" + itemName
-					+ ", itemPrice=" + itemPrice + ", exempt=" + exempt
-					+ ", taxRate=" + taxRate + "]";
-		}
-//applying tax to each item
-		public BigDecimal getTax() {
-			//holding total sales tax for this product
-			taxRate = new BigDecimal(0);
-			BigDecimal taxItemPrice = new BigDecimal(0);
-			
-			if (exempt == false){
-				//add operator only works with primitives, so used BigDecimal add method to add 10% tax
-				taxItemPrice = itemPrice.multiply(BigDecimal.valueOf(.10));
-				//taxRate.add(taxRate = new BigDecimal(0););
-			}//close if/else
-			
-			if (imprt == true){
-				//taxItemPrice = itemPrice.multiply(BigDecimal.valueOf(.05));
-				taxItemPrice = taxItemPrice.add(itemPrice.multiply(BigDecimal.valueOf(.05)));
-				//taxRate.add(BigDecimal.valueOf(.05));d
-			}
-			//multiply price times tax to get sales tax for each item
-			
-			//System.out.println(taxItemPrice);
-			//convert BigDecimal to double
-			              //  taxItemPrice = taxItemPrice.setScale(2, BigDecimal.ROUND_CEILING);
-			                taxItemPrice  =  new BigDecimal(Math.ceil(taxItemPrice.doubleValue() * 20) / 20);
-			                taxItemPrice = taxItemPrice.setScale(2, BigDecimal.ROUND_HALF_UP);
-			                //taxItemPrice.setScale(2, RoundingMode.HALF_UP);
-			return taxItemPrice;
-			
-		}
-			
-	
-		}//close Product class bracket
+}// close Product class bracket
 
